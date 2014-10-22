1a2af3f915c3a701e4e6bc2892b64c1315e94e09
Henriette Kopstad <henriette.kopstad@gmail.com>
2014-10-21 22:50:14 -0700

    Worker and Entities done. Task 3 - ReadData finished

--- src/main/java/com/henriette/FirstGAEApp/Enqueue.java
+++ src/main/java/com/henriette/FirstGAEApp/Enqueue.java
@@ -17,11 +17,12 @@ public class Enqueue extends HttpServlet {
  protected void doPost(HttpServletRequest request, HttpServletResponse response)
          throws ServletException, IOException {
      String key = request.getParameter("key");
+     String value = request.getParameter("value");
 
      // Add the task to the default queue.
      Queue queue = QueueFactory.getDefaultQueue();
-     queue.add(withUrl("/worker").param("key", key));
+     queue.add(withUrl("/worker").param("key", key).param("value", value));
 
-     response.sendRedirect("/done.html");
+     response.sendRedirect("/test");
  }
 }
--- src/main/java/com/henriette/FirstGAEApp/Worker.java
+++ src/main/java/com/henriette/FirstGAEApp/Worker.java
@@ -1,17 +1,33 @@
 package com.henriette.FirstGAEApp;
 import java.io.IOException;
+import java.util.Date;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.google.appengine.api.datastore.*;
 //The Worker servlet should be mapped to the "/worker" URL.
 public class Worker extends HttpServlet {
  protected void doPost(HttpServletRequest request, HttpServletResponse response)
          throws ServletException, IOException {
-     String key = request.getParameter("key");
-     // Do something with key.
+	 String key = request.getParameter("key");
+     String value =request.getParameter("value");
+     Date date = new Date();
+     
+     Entity entity = new Entity("TaskData",key);
+     entity.setProperty("value", value);
+     entity.setProperty("date", date);
+
+     DatastoreService datastore = DatastoreServiceFactory.getDatastoreService();
+     datastore.put(entity);
+
+     response.sendRedirect("/done.html");
+     
+     //to create an entity of type TaskData and store it in the datastore using the 
+     //key value that was passed in. These entities should have properties "value" and 
+     //"date" which hold the string value passed in and the current date value, respectively.
  }
 }
 
